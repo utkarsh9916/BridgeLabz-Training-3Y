0f61cb0eee8dab14a204e45490c91678d4664be7
utkarsh9916 <maheshwari.utkarsh2006@gmail.com>
2025-09-04 23:43:08 +0530

    folder name change

---
+++ 11_AUG_assignment/SimpleInterest.java
@@ -0,0 +1,6 @@
+principal = float(input("Enter Principal: "))
+rate = float(input("Enter Rate of Interest: "))
+time = float(input("Enter Time (in years): "))
+
+simple_interest = (principal * rate * time) / 100
+print("Simple Interest =", simple_interest)
